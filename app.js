a0fceb34ffcc951abbc2edb11fe505feca576455
baufakhran <baufakhran@gmail.com>
2020-02-21 11:23:44 +0700

    add method fetch question

--- app.js
+++ app.js
@@ -21,61 +21,68 @@ app.use(cors())
 app.use(errHandler)
 
 io.on('connection', function (socket) {
-    console.log('a user connected')
-    socket.on('addRooms', (payload) => {
+  console.log('a user connected')
+  socket.on('addRooms', (payload) => {
 
-        let dataRoom = {
-            name: payload.roomName,
-            totalPlayer: 0
-        }
+    let dataRoom = {
+      name: payload.roomName,
+      totalPlayer: 0
+    }
+    console.log(dataRoom);
 
-        Room.create(dataRoom)
-            .then(room => {
-                console.log('success adding room')
-                io.emit('roomCreated', room)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    })
+    Room.create(dataRoom)
+      .then(room => {
+        console.log('success adding room')
+        io.emit('roomCreated', room)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  })
 
-    socket.on('fetchRooms', () => {
-        Room
-            .findAll()
-            .then(rooms => {
-                socket.emit('showRooms', rooms)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    })
+  socket.on('fetchRooms', () => {
+    Room
+      .findAll()
+      .then(rooms => {
+        socket.emit('showRooms', rooms)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  })
 
-    socket.on('joinRoom', (payload) => {
-        
-        socket.join(payload.id, (err) => {
-            Room
-                .findByPk(payload.id)
-                .then(room => {
-                    console.log(room);
-                    
-                    let newTotalPlayer = room.totalPlayer + 1
-                    return Room.update({ totalPlayer: newTotalPlayer }, {
-                        where: {
-                            id: payload.id
-                        }
-                    })
-                })
-                .then(room => {
-                    io.to(payload.id).emit('someoneJoined', payload.username)
-                })
-                .catch(err => {
-                    console.log(err)
-                })
+  socket.on('joinRoom', (payload) => {
 
+    socket.join(payload.id, (err) => {
+      Room
+        .findByPk(payload.id)
+        .then(room => {
+          console.log(room);
+
+          let newTotalPlayer = room.totalPlayer + 1
+          return Room.update({ totalPlayer: newTotalPlayer }, {
+            where: {
+              id: payload.id
+            }
+          })
+        })
+        .then(room => {
+          io.to(payload.id).emit('someoneJoined', payload.username)
+        })
+        .catch(err => {
+          console.log(err)
         })
     })
+  })
+
+  socket.on('fetchQuestions', (payload) => {
+    let numQuestion = 5
+    let sendQuestions = getRandomQuestions(questions, numQuestion)
+    io.to(payload.id).emit('showQuestions', sendQuestions)
+  })
+
 });
 
 server.listen(3000, function () {
-    console.log('listening on *:3000');
+  console.log('listening on *:3000');
 });
